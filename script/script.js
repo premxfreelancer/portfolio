79af89f29bb6607d8ba2b7d5bea46bf29abd1164
Prem <premruidas123@gmail.com>
2023-12-25 10:37:53 +0530

    1

---
+++ script/script.js
@@ -0,0 +1,9 @@
+var statusview = document.getElementById("statusview");
+
+function viewstatusfun() {
+    statusview.style.display = 'block';
+}
+
+function closestatusfun() {
+    statusview.style.display = 'none';
+}
