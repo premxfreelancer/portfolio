7610ea00a7a33c166fac2ae35c714fca2526be53
Prem <premruidas123@gmail.com>
2023-12-25 11:24:28 +0530

    4

--- script/script.js
+++ script/script.js
@@ -7,3 +7,10 @@ function viewstatusfun() {
 function closestatusfun() {
     statusview.style.display = 'none';
 }
+
+document.onreadystatechange = function () {
+    if (document.readyState === "complete") {
+        // Hide the loader when the page is fully loaded
+        document.querySelector('.loader-wrapper').style.display = 'none';
+    }
+};
